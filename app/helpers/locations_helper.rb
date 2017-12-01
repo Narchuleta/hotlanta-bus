b11113887db5c0e9a2bc14bb61821bf807669d89
Nicole Archuleta <archuletanola@gmail.com>
2017-12-01 17:13:31 +0000

    Add geocoder

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,19 @@
 module LocationsHelper
+  def fetch_buses_from_api(bus_api_url)
+    raw_http = Net::HTTP.get_response(URI.parse(bus_api_url))
+    bus_data = raw_http.body
+    
+    JSON.parse(bus_data)
+  end
+  def is_nearby?(user_latitude, user_longitude, bus_latitude, bus_longitude)
+    max_distance = 0.01 #about 1/2 mile
+    
+    difference_latitudes = user_latitude - bus_latitude.to_f
+    difference_longitudes = user_longitude - bus_longitude.to_f
+    
+    distance = Math.sqrt(difference_latitudes ** 2 + difference_longitudes ** 2)
+    
+    distance <= max_distance
+  end
+
 end
